e8a9a1a3388c74d6585d58aebade43fddafdcfd9
LucyBR123 <122480492+LucyBR123@users.noreply.github.com>
2023-01-17 23:26:50 -0600

    Update select_a.php

--- select_a.php
+++ select_a.php
@@ -27,16 +27,16 @@
 
             <label hidden for="table">Tabla </label>
             <select hidden id="table" name="table">
-                <option <?php if(isset($_POST["table"]) && $_POST["table"] == "conductores") echo "selected"?> value="conductores">Conductores</option>
-                <option <?php if(isset($_POST["table"]) && $_POST["table"] == "licencias") echo "selected"?> value="licencias">Licencias</option>
-                <option <?php if(isset($_POST["table"]) && $_POST["table"] == "multas") echo "selected"?> value="multas">Multas</option>
+            <Div class="Tabla"><option class="Tabla" <?php if(isset($_POST["table"]) && $_POST["table"] == "conductores") echo "selected"?> value="conductores">Conductores</option></Div>
+            <Div class="linea"><option <?php if(isset($_POST["table"]) && $_POST["table"] == "licencias") echo "selected"?> value="licencias">Licencias</option></Div>
+            <Div class="linea"><option <?php if(isset($_POST["table"]) && $_POST["table"] == "multas") echo "selected"?> value="multas">Multas</option></Div>
                 <option <?php if(isset($_POST["table"]) && $_POST["table"] == "oficiales") echo "selected"?> value="oficiales">Oficiales</option>
                 <option <?php if(isset($_POST["table"]) && $_POST["table"] == "propietarios") echo "selected"?> value="propietarios">Propietarios</option>
                 <option <?php if(isset($_POST["table"]) && $_POST["table"] == "tarjetas_circulacion") echo "selected"?> value="tarjetas_circulacion">Tarjetas de Circulación</option>
                 <option <?php if(isset($_POST["table"]) && $_POST["table"] == "vehiculos") echo "selected"?> value="vehiculos">Vehiculos</option>
                 <option <?php if(isset($_POST["table"]) && $_POST["table"] == "verificaciones") echo "selected"?> value="verificaciones">Verificaciones</option>
             </select>
-            <br>
+            
 
             <input type="submit" name="select" value="Aceptar" class="BotG">
 
@@ -53,13 +53,18 @@
 
     function DisplayData(mysqli_result $data, string $table): string
     {
-        if ($data->num_rows == 0) return "No se ha encontrado ningun resultado.";
+
+
+            
+        if ($data->num_rows == 0) return $output = "No se ha encontrado ningun resultado.";
+        
+
 
         $row = mysqli_fetch_row($data);
 
-        $output = "<table border='1'>";
+        $output = "<table  class=\"TabC\"> ";
         foreach ($data as $key => $var) {
-            $output .= "<tr>";
+            $output .= "<tr class=\"TabC2\" >";
             $output .= DisplayRow($var, $key, $table);
             $output .= "</tr>";
         }
@@ -72,19 +77,19 @@
     {
         $output = "";
         if ($key !== 0) {
-            $output .= "<form action=\"./select_a.php\" method=\"post\">";
-            $output .= "<input type=\"hidden\" name=\"table\" value=\"$table\"/>";
+            $output .= "<form action=\"./select_a.php\" method=\"post\" class=\"po123\">";
+            $output .= "<input type=\"hidden\" name=\"table\" value=\"$table\" class=\"FieG2\"/>";
         }
 
         foreach ($row as $k => $v) {
             if ($key === 0) {
-                $output .= "<th><strong>" . $k . "</strong></th>";
+                $output .= "<th><strong class=\"TitT\">" . $k . "</strong></th>";
             } else {
                 $output .= "<td>";
                 if ($k === "id") {
-                    $output .= "<input type=\"hidden\" type=\"text\" id=\"$k\" name=\"$k\" value=\"$v\">$v";
+                    $output .= "<br/><input type=\"hidden\" type=\"text\" id=\"$k\" name=\"$k\" value=\"$v\" class=\"FieG2\" >$v";
                 } else {
-                    $output .= "<input type=\"text\" id=\"$k\" name=\"$k\" value=\"$v\">";
+                    $output .= "<input type=\"text\" id=\"$k\" name=\"$k\" value=\"$v\" class=\"FieG2\">";
                 }
                 $output .= "</td>";
             }
@@ -95,9 +100,9 @@
             $output .= DisplayRow($row, 1, $table);
         } else {
             $output .= "<td>";
-            $output .= "<input type=\"submit\" name=\"update\" value=\"Update\">";
+            $output .= "<input type=\"submit\" name=\"update\" value=\"Update\" class=\"BotC\"> " ;
             $output .= "</td><td>";
-            $output .= "<input type=\"submit\" name=\"delete\" value=\"Delete\">";
+            $output .= "<input type=\"submit\" name=\"delete\" value=\"Delete\" class=\"BotC\"> ";
             $output .= "</td></form>";
         }
 
