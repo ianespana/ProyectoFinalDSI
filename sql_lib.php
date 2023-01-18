e0868c1b6e8952b3ba7f4a261b3a1cf97d137ee7
Shakdolow <saultigre9@gmail.com>
2023-01-18 04:25:37 -0600

    Update sql_lib.php

--- sql_lib.php
+++ sql_lib.php
@@ -55,3 +55,92 @@ function InsertArray(string $table, array $array, array $exclude): InsertResults
     Close($conn);
     return $results;
 }
+
+// function createXMLFile($vehiculos){
+
+// $conn = Connect();
+// $vehiculo = array();
+// $sql = "SELECT id, niv, tipo, marca, modelo, numero_serie, clase, tipo_combustible, caballos_fuerza, tipo_carroceria, color, transmision, serie_motor, capacidad FROM vehiculos"
+
+// if($result = $mysqli->query($sql)){
+//     $vehiculo = $result->fetch_all(MYSQLI_ASSOC);
+
+//     if(count($vehiculos)){
+        
+//         $xmlFile = createXMLFile($vehiculos);
+//         echo "Archivo generado exitosamente";
+//     } else {
+//         echo "No se ha encontrado un registro";
+//     }
+
+//     $result->free();
+
+// }
+// mysqli->close();
+
+
+
+
+//     $filepath = '../proyecto_final/vehiculos.xml';
+
+//     $dom = new DOMDocument('1.0', 'utf-8');
+//     $root = $dom->createElement('vehiculos');
+
+//     foreach ($vehiculos as $vehiculo){
+
+//         $vehiculoElemento = $dom->createElement('vehiculo');
+//         $vehiculoElemento->setAttribute('id',$vehiculo['id']);
+
+//         $niv = $dom->createElement('niv', ($vehiculo['niv']));
+//         $vehiculoElemento->appendChild($niv);
+
+//         $tipo = $dom->createElement('tipo', ($vehiculo['tipo']));
+//         $vehiculoElemento->appendChild($tipo);
+        
+//         $marca = $dom->createElement('marca', ($vehiculo['marca']));
+//         $vehiculoElemento->appendChild($marca);
+        
+//         $modelo = $dom->createElement('modelo', ($vehiculo['modelo']));
+//         $vehiculoElemento->appendChild($modelo);
+        
+//         $numero_serie = $dom->createElement('numero_serie', ($vehiculo['numero_serie']));
+//         $vehiculoElemento->appendChild($numero_serie   );
+        
+//         $clase = $dom->createElement('clase', ($vehiculo['clase']));
+//         $vehiculoElemento->appendChild($clase);
+        
+//         $tipo_combustible = $dom->createElement('tipo_combustible', ($vehiculo['tipo_combustible']));
+//         $vehiculoElemento->appendChild($tipo_combustible);
+        
+//         $numero_cilindros = $dom->createElement('numero_cilindros', ($vehiculo['numero_cilindros']));
+//         $vehiculoElemento->appendChild($numero_cilindros);
+        
+//         $caballos_fuerza = $dom->createElement('caballos_fuerza', ($vehiculo['caballos_fuerza']));
+//         $vehiculoElemento->appendChild($caballos_fuerza);
+        
+//         $tipo_carroceria = $dom->createElement('tipo_carroceria', ($vehiculo['tipo_carroceria']));
+//         $vehiculoElemento->appendChild($tipo_carroceria);
+        
+//         $color = $dom->createElement('color', ($vehiculo['color']));
+//         $vehiculoElemento->appendChild($color);
+        
+//         $transmision = $dom->createElement('transmision', ($vehiculo['transmision']));
+//         $vehiculoElemento->appendChild($transmision);
+        
+//         $serie_motor = $dom->createElement('serie_motor', ($vehiculo['serie_motor']));
+//         $vehiculoElemento->appendChild($serie_motor);
+        
+//         $capacidad = $dom->createElement('capacidad', ($vehiculo['capacidad']));
+//         $vehiculoElemento->appendChild($capacidad);
+
+//         $root->appendChild($vehiculoElemento);
+//     }
+
+//     $dom->appendChild($root);
+    
+//     if($dom->save($filePath)){
+
+//         return $filepath;
+//     }
+//     return false;
+// }
